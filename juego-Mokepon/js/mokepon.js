8319fd03e94cd7ad4f3cf26e2887257ae41de9b2
Antonio Poumian <rivant_09@hotmail.com>
2023-06-29 20:20:43 -0600

    Quién ganó el juego

--- juego-Mokepon/js/mokepon.js
+++ juego-Mokepon/js/mokepon.js
@@ -97,6 +97,16 @@ function combate() {
     vidasJugador--;
     spanVidasJugador.innerHTML = vidasJugador;
   }
+
+  revisarVidas();
+}
+
+function revisarVidas() {
+  if (vidasEnemigo == 0) {
+    crearMensajeFinal("FELICITACIONES! Ganaste :)");
+  } else if (vidasJugador == 0) {
+    crearMensajeFinal("Lo siento, perdiste :(");
+  }
 }
 
 function crearMensaje(resultado) {
@@ -110,6 +120,15 @@ function crearMensaje(resultado) {
     ataqueEnemigo +
     "- " +
     resultado;
+
+  sectionMensajes.appendChild(parrafo);
+}
+
+function crearMensajeFinal(resultadoFinal) {
+  let sectionMensajes = document.getElementById("mensajes");
+
+  let parrafo = document.createElement("p");
+  parrafo.innerHTML = resultadoFinal;
 
   sectionMensajes.appendChild(parrafo);
 }
