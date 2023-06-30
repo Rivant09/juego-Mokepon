e5258b7890a9af3d51d76ebf0bc1c260c742b97e
Antonio Poumian <rivant_09@hotmail.com>
2023-06-29 19:15:38 -0600

    Dónde ubicar la etiqueta script? Conectando HTML con JavaScript

---
+++ juego-Mokepon/js/mokepon.js
@@ -0,0 +1,6 @@
+function seleccionarMascotaJugador() {
+  alert("SELECCIONASTE TU MASCOTA");
+}
+
+let botontMascotaJugador = document.getElementById("boton-mascota");
+botontMascotaJugador.addEventListener("click", seleccionarMascotaJugador);
